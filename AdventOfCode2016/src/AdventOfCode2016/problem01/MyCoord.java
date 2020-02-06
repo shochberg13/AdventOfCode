7483960454d80888cd4dab39bef0ed8c8e7b3035
Seth Hochberg <shochberg13@gmail.com>
2020-02-06 16:55:39 -0500

    Changed the package names to better organize files

---
+++ AdventOfCode2016/src/AdventOfCode2016/problem01/MyCoord.java
@@ -0,0 +1,40 @@
+package AdventOfCode2016.problem01;
+
+class MyCoord{
+    private int X;
+    private int Y;
+
+    public MyCoord() {
+        this(0,0);
+    }        
+    public MyCoord(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+    public int getX() {
+        return X;
+    }
+    public int getY() {
+        return Y;
+    }
+    public void setX(int X) {
+        this.X = X;
+    }
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+    
+    @Override
+    public boolean equals(Object object){
+    	if (object == null || getClass() != object.getClass()){
+    		return false;
+    	}
+    	
+    	MyCoord compared = (MyCoord) object;
+    	if (this.X != compared.X || this.Y != compared.Y){
+    		return false;
+    	}
+    	
+    	return true;
+    }
+}
---
+++ AdventOfCode2016/src/AdventOfCode2016/problem02/Main.java
@@ -0,0 +1,10 @@
+package AdventOfCode2016.problem02;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+		Problem02a problem02a = new Problem02a();
+		problem02a.runProgram();
+	}
+}
